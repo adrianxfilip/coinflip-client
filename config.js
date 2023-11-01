73d5bcf6002c0e01f81e0f2aba65da3427a7c084
adixfilip <adrianxfilip@gmail.com>
2023-11-01 14:00:40 +0200

    Updates

---
+++ config.js
@@ -0,0 +1,8 @@
+var SERVER_URL = "http://buratancunebunu.home.ro:4000"
+
+//DO NOT CHANGE FIRST AND LAST VALUES (0 AND 999999)
+var FILTERS = [[0,5],[5,10],[10,25],[25,50],[50,100],[100,999999]]
+
+var CURRENCY = "RON"
+
+var IFRAME_ORIGIN = "http://localhost:3001"
