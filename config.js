2a01e49321e4a09c93560be0253d14b0239b198e
adixfilip <adrianxfilip@gmail.com>
2024-01-15 15:48:26 +0200

    Updates

--- config.js
+++ config.js
@@ -1,9 +1,20 @@
-var SERVER_URL = "https://coinflip-server.adaptable.app"
+//var SERVER_URL = "https://coinflip-server.adaptable.app"
 //var SERVER_URL = "http://buratancunebunu.home.ro:4000"
+var SERVER_URL = "192.168.1.186:4000"
 
 //DO NOT CHANGE FIRST AND LAST VALUES (0 AND 999999)
 var FILTERS = [[0,5],[5,10],[10,25],[25,50],[50,100],[100,999999]]
 
 var CURRENCY = "RON"
+
+var TEXTS = {
+    balance : "Balanta",
+    openGames : "Jocuri",
+    createGame : "Pariaza",
+    joinGame : "Joaca",
+    restrictedChat : "Accesul la chat este restrictionat",
+    heads : "Cap",
+    tails : "Pajura"
+}
 
 var IFRAME_ORIGIN = "http://localhost:3001"
